a0495072bf16fa7080517722382dacb73f9bb145
HUZAIFA HATIM <90339379+huzaifahatim@users.noreply.github.com>
2022-08-24 04:31:06 +0500

    Test

---
+++ NFT-Test.sol
@@ -0,0 +1,130 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TheStripesNFT is ERC721Enumerable, Ownable {
+    using Strings for uint256;
+
+    string public baseURI;
+    string public baseExtension = ".json";
+    uint256 public cost = 0.0002 ether;
+    uint256 public maxSupply = 100;
+    bool public paused = false;
+
+    constructor(string memory _name, string memory _symbol, string memory _initBaseURI) 
+    ERC721(_name, _symbol) {
+        setBaseURI(_initBaseURI);
+    }
+
+
+
+
+
+    
+
+
+
+    // internal
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
+    }
+
+    // public
+    function mint(address _to, uint256 _mintAmount) public payable {
+        uint256 supply = totalSupply();
+        require(!paused);
+        require(_mintAmount > 0);
+        require(supply + _mintAmount <= maxSupply);
+ 
+        
+        
+         if (msg.sender != owner()) {
+            require(_mintAmount <= balanceOf(msg.sender),"Exceed Your Mint Limit" );
+            require(_mintAmount <= 80, "Exceed Your Mint Limit" ); 
+            
+        }
+
+        else {
+            require(_mintAmount <= balanceOf(msg.sender),"Exceed Your Mint Limit" );
+            require(_mintAmount <= 20,"Exceed Your Mint Limit" );
+        }
+
+
+        require(msg.value >= cost * _mintAmount);
+
+        
+        
+        for (uint256 i = 1; i <= _mintAmount; i++) {
+            _safeMint(_to, supply + i);
+        }
+    }
+
+    function walletOfOwner(address owner_)
+        public
+        view
+        returns (uint256[] memory)
+    {
+        uint256 ownerTokenCount = balanceOf(owner_);
+        uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+        for (uint256 i; i < ownerTokenCount; i++) {
+            tokenIds[i] = tokenOfOwnerByIndex(owner_, i);
+        }
+        return tokenIds;
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+
+        string memory currentBaseURI = _baseURI();
+        return
+            bytes(currentBaseURI).length > 0
+                ? string(
+                    abi.encodePacked(
+                        currentBaseURI,
+                        tokenId.toString(),
+                        baseExtension
+                    )
+                )
+                : "";
+    }
+
+    //only owner
+    function setCost(uint256 _newCost) public onlyOwner {
+        cost = _newCost;
+    }
+
+
+    function setBaseURI(string memory _newBaseURI) public onlyOwner {
+        baseURI = _newBaseURI;
+    }
+
+    function setBaseExtension(string memory _newBaseExtension)
+        public
+        onlyOwner
+    {
+        baseExtension = _newBaseExtension;
+    }
+
+    function pause(bool _state) public onlyOwner {
+        paused = _state;
+    }
+
+    function withdraw() public payable onlyOwner {
+        (bool success, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(success);
+    }
+}
